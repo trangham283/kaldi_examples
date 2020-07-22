32bbc3226f6ec306f95f99504c356c9eeea3f383
Trang Tran <trangham283@gmail.com>
2020-07-21 23:37:47 -0700

    more notes

--- my_data/make_list_files.sh
+++ my_data/make_list_files.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-task=da
+task=pa
 split=dev
 datadir=/s0/ttmt001/utterances/$task/$split
 outdir=my_data/$task/$split
@@ -13,6 +13,6 @@ sed -e 's?.*/??' -e 's?.wav??' $outdir/wav.flist | paste - $outdir/wav.flist > $
 awk '{print $1}' $outdir/wav.scp > $outdir/utt2spk 
 
 # Then some vim magic in utt2spk:
-# - copy over to second column
-# - delete turn_id from second column
+# - copy over to second column: CTRL+V all; then SHIFT+I, then CTRL+P; then ESC
+# - delete turn_id from second column: CTRL+V last 5 characters (stuff from _*); D
 
